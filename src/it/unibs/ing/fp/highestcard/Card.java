0b62844d32ddf2ea032c616509e3efa85dc01f97
Simone Cavicchioli <s.cavicchioli@studenti.unibs.it>
2016-05-18 20:33:45 +0200

    New version of Class Card

--- src/it/unibs/ing/fp/highestcard/Card.java
+++ src/it/unibs/ing/fp/highestcard/Card.java
@@ -5,44 +5,83 @@ package it.unibs.ing.fp.highestcard;
  * <p>
  * 
  * @author Simone Cavicchioli
- * @version v1.1
+ * @version v2.0
  * @since 2016-05-03
  */
 
 public class Card {
     private String suit;
     private String value;
+    private int index;
 
     /**
      * Constructor.
      * @param suit - The suit of Card
      * @param value - The value of Card
      */
-    public Card(String suit, String value) {
+    public Card(String suit, String value, int index) {
         this.suit = suit;
         this.value = value;
+        this.setIndex(index);
     }
     
-    //public Card() { }
+    // Getters
     
+    /**
+	 * @return the suit
+	 */
     public String getSuit() {
         return suit;
     }
     
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-    
+    /**
+     * @return the value
+     */
     public String getValue() {
         return value;
     }
     
+    /**
+	 * @return the index
+	 */
+	public int getIndex() {
+		return index;
+	}
+	
+	// Setters
+    
+    /**
+     * @param suit - The suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+    
+    /**
+     * @param value - The value to set
+     */
     public void setValue(String value) {
         this.value = value;
     }
+    
+	/**
+	 * @param index - The index to set
+	 */
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	
+	/**
+	 * Returns the difference between two Card.
+	 * @param other - The other card to compare
+	 * @return the difference
+	 */
+	public int compareTo(Card other) {
+		return this.getIndex() - other.getIndex();
+	}
 
     @Override
     public String toString() {
-        return "\n"+value + " of "+ suit;
-    }
+        return "\n"+ value + " of "+ suit;
+    }	
 }
