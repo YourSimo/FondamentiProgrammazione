614aa285967a458cc7c90f7b0e6cc9806684b0c2
Simone Cavicchioli <s.cavicchioli@studenti.unibs.it>
2016-05-18 21:49:54 +0200

    Update Class HighestCard

--- src/it/unibs/ing/fp/highestcard/HighestCard.java
+++ src/it/unibs/ing/fp/highestcard/HighestCard.java
@@ -1,6 +1,8 @@
 package it.unibs.ing.fp.highestcard;
 
+import it.unibs.ing.fp.hello.Hello;
 import it.unibs.ing.fp.library.InputData;
+import it.unibs.ing.fp.library.Menu;
 
 /**
  * <h1> Class HighestCardMain </h1>
@@ -12,74 +14,69 @@ import it.unibs.ing.fp.library.InputData;
  */
 
 public class HighestCard {
-	private final static String [] ITA_VALUES = {"A","2","3","4","5","6","7","Knave","Knight","King"};
-    private final static String [] ITA_SUITS = {"Swords", "Cups", "Coins", "Clubs"};
+	private final static String NOMEIT = "ITALIANE";
+	private final static String [] IT_VALUES = {"A","2","3","4","5","6","7","Knave","Knight","King"};
+    private final static String [] IT_SUITS = {"Swords", "Cups", "Coins", "Clubs"};
+    
+    
+    private final static String NOMEFR = "FRANCESI";
+    private final static String [] FR_VALUES = {"A","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+    private final static String [] FR_SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
 	
-	private final static String INTRO = "";
+	private final static String INTRO = "BENVENUTO AL GIOCO DELLA CARTA PIU' ALTA";
 	
-	private final static String MSG_BALANCE = "";
+	private final static String INTESTA_TIPI = "SELEZIONE DEL TIPO DI CARTE DA USARE";
+	private final static String MSG_START = "INIZIO PARTITA";
+	
+	private final static String MSG_NOME = "Inserisci il tuo nome:";
+	private final static String MSG_BALANCE = ", con quanti euro vuoi giocare? ";
 	private final static double MIN_BALANCE = 0.01;
 	
-	private final static String FIRST_CHOISE = "Quale tipo di carte preferisci: Italiane [I] o Francesi [F] ?";
-	private final static String VALID_CHAR = "IF";
+	public final static String [] NOMI_ESISTENTI = {NOMEIT, NOMEFR};
+	
+	
+	
+	// private final static String FIRST_CHOISE = "Quale tipo di carte preferisci: Italiane [I] o Francesi [F] ?";
+	// private final static String VALID_CHAR = "IF";
 
 	private final static String CHOISE_ERR = "Attenzione: inserimento errato";
 	
-	public static Gamer makeGamer() {
-		double balance = InputData.readDoubleWithMin(MSG_BALANCE, MIN_BALANCE);
-		return new Gamer(balance);
-	}
-	
-	public static void firstChoise() {
-		char operation = InputData.readCharLimitedNotSensitive(FIRST_CHOISE, VALID_CHAR);
-		switch (operation) {
-		case 'i' :
-		case 'I' : 
-			// ItalianDeck deckIt = new ItalianDeck();
-			String [] suits = ITA_SUITS;
-			String [] values = ITA_VALUES;
-			break;
-		case 'f' :
-		case 'F' :
-			// FrenchDeck deckF = new FrenchDeck();
-			break;
-		default :
-			System.out.println(CHOISE_ERR);
-		}
-		// return new Deck(values, suits);
-	}
-	
-	public static void next() {
+	private static void play(Deck type) {
+		System.out.println(MSG_START);
+		String nomeUtente = InputData.readStringNotEmpty(MSG_NOME);
+		double soldiIniziali = InputData.readDoubleWithMin(nomeUtente + MSG_BALANCE, MIN_BALANCE);
 		
+		Player player = new Player(nomeUtente, soldiIniziali);
+		Game game = new Game(player, type);
+		game.esegui();
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Saluto
+		Hello.sayHello(INTRO);
 		
-		// firstChoise
-		/*
-        FrenchDeck fdeck = new FrenchDeck();
-
-        //print out the deck.
-        System.out.println(fdeck.getDeck());
-        fdeck.shuffleDeck();
-        System.out.println(fdeck.getDeck());
-        fdeck.shuffleDeck();
-        System.out.println(fdeck.getDeck());
-        fdeck.getRandomCard();
-        */
-		if(true) {
-			String [] values = ITA_VALUES;
-			String [] suits = ITA_SUITS;
-		}
+		Deck itDeck = new Deck(IT_VALUES, IT_SUITS);
+		Deck frDeck = new Deck(FR_VALUES, FR_SUITS);
 		
-		/*
-		 * Deck deck = new Deck(values, suits);
-		 * System.out.println(itaDeck.getDeck());
-		 * System.out.println(itaDeck.getRandomCard());
-		 */
+		Menu mainMenu = new Menu(INTESTA_TIPI, NOMI_ESISTENTI);
 		
+		boolean finish = false;
+		do {
+			int selezione = mainMenu.choise();
+			switch (selezione) {
+				case 1: 
+					play(itDeck);
+					break;
+				case 2: 
+					play(frDeck);
+					break;
+				case 0:
+					finish = true;
+					break;
+				default:
+					System.out.println(CHOISE_ERR);
+			} 
+		} while (!finish);
     }
 }
